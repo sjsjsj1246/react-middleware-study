47ad1e1528b7573d64b060d62d86f211e0fca766
Hwang In Seo <sjsjsj1246@gmail.com>
2021-01-23 16:11:39 +0900

    init project add middleware

---
+++ src/containers/CounterContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Counter from "../components/Counter";
+import { decrease, increase } from "../modules/counter";
+
+const CounterContainer = (props) => {
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+
+  const onDecrease = () => {
+    dispatch(decrease());
+  };
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+export default CounterContainer;
